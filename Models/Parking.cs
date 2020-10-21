ebeaa05ada1ec0c34684b38fe9aa5102057c0feb
VladGrushevoi <vlad.grush.99@gmail.com>
2020-10-21 16:34:59 +0300

    nothing to change

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -5,16 +5,5 @@ namespace Models
         public int Id {get; set;}
         public string Name {get; set;}
         public string Description{get; set;}
-
-        public Parking(int id, string name, string description)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Description = description;
-        }
-
-        public Parking(){
-
-        }
     }
 }
