da76f48f51cd5bd74addd424cfde57114e11c0eb
VladGrushevoi <vlad.grush.99@gmail.com>
2020-10-22 18:16:39 +0300

    third migration for place and create entity

--- Migrations/20201022141707_initPlace.Designer.cs
+++ Migrations/20201022141707_initPlace.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AutoParking_backend;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutoParking_backend.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20201022141707_initPlace")]
+    partial class initPlace
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
