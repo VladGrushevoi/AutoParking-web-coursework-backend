abed48c576cf693cfce9beaaf949990311162559
VladGrushevoi <vlad.grush.99@gmail.com>
2020-10-25 17:49:00 +0200

    add/create place search model

---
+++ Models/SearchModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Models
+{
+    public class SearchModel
+    {
+        public int? TypePlace {get; set;}
+        public int? TypeCar {get; set;}
+        public DateTime? StartPeriod {get; set;}
+        public DateTime? EndPeriod {get; set;}
+    }
+}
