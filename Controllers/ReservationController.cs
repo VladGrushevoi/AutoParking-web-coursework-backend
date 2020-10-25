97781f6cf3b68d76dc127dcd27271a0af0babd9b
VladGrushevoi <vlad.grush.99@gmail.com>
2020-10-25 17:44:15 +0200

    add resrvation controller

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace Controller
+{
+    [ApiController]
+    [Route("api")]
+    public class ReservationController : ControllerBase
+    {
+        private ReservationService reservationService;
+
+        public ReservationController(ReservationService service)
+        {
+            this.reservationService = service;
+        }
+
+        [HttpGet("getreserv")]
+        public ActionResult<List<Reserv>> GetAllReserv()
+        {
+            return Ok(reservationService.GetallReserv());
+        }
+
+        [HttpGet("search/freeplace")]
+        public ActionResult<List<Place>> GetAllFreePlace()
+        {
+            return Ok(reservationService.FreePlace(new SearchModel{TypeCar=1, TypePlace=2}));
+        }
+
+        [HttpGet("search/parking/{id}/place")]
+        public ActionResult<List<Place>> GetAllFreePlaceByParking(int id )
+        {
+            return Ok(reservationService.FreePlaceByParking(id, new SearchModel{TypeCar=1, TypePlace=2}));
+        }
+
+        [HttpPost("reserv")]
+        public ActionResult<Reserv> AddReserv([FromBody] ReservModel rm)
+        {
+            return Created("Created reserv",reservationService.ReservPlace(rm).Result);
+        }
+    }
+}
