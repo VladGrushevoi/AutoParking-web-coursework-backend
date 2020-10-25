c1fd077beb166087f54d7897db5a82f5fef9f2cc
VladGrushevoi <vlad.grush.99@gmail.com>
2020-10-25 17:46:05 +0200

    add new migrations

---
+++ Migrations/20201024111305_reserv.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoParking_backend.Migrations
+{
+    public partial class reserv : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reservations",
+                columns: table => new
+                {
+                    ReservId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClientId = table.Column<int>(nullable: false),
+                    PlaceId = table.Column<int>(nullable: false),
+                    StartPeriod = table.Column<DateTime>(nullable: false),
+                    EndPeriod = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reservations", x => x.ReservId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reservations");
+        }
+    }
+}
--- Migrations/20201024154041_some.Designer.cs
+++ Migrations/20201024154041_some.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AutoParking_backend;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutoParking_backend.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20201024154041_some")]
+    partial class some
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
